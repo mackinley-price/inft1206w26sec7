750307d6517b8f2058e8cef26275b50f3f69f973
mackinley-price <mackinley.price@dcmail.ca>
2026-04-04 17:37:14 -0400

    reverts back to original finished project
    was working on the wrong file - reverted back to intended final draft

--- assignment4/part3/script.js
+++ assignment4/part3/script.js
@@ -23,22 +23,14 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Shape{
-    constructor(x, y, velX, velY) {
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
-    }
-}
-
-class Ball extends Shape{
-    
-  constructor(x, y, velX, velY, color, size) {
-    super(x,y,velX,velY);
     this.color = color;
     this.size = size;
-    this.exists = true;
     }
 
     draw() {
@@ -70,7 +62,7 @@ class Ball extends Shape{
   }
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball) && ball.exists) {
+      if (this !== ball) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -82,40 +74,6 @@ class Ball extends Shape{
     }
   }
 }
-
-class EvilCircle extends Shape{
-    constructor(x,y){
-        super(x,y,20,20);
-        this.color = "white";
-        this.size = 10; 
-    
-    window.addEventListener("keydown", (e) => {
-        switch (e.key) {
-            case "a":
-            this.x -= this.velX;
-            break;
-            case "d":
-            this.x += this.velX;
-            break;
-            case "w":
-            this.y -= this.velY;
-            break;
-            case "s":
-            this.y += this.velY;
-            break;
-            }
-        });
-    }
-
-    draw() {
-    ctx.beginPath();
-    ctx.lineWidth = 3
-    ctx.strokeStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.stroke();
-    }
-}
-
 
 const testBall = new Ball(50, 100, 4, 4, "blue", 10);
 testBall.x;
