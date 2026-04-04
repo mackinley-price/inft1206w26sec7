9b5de102999c2a589591a382bdf63f9bbbcf984d
mackinley-price <mackinley.price@dcmail.ca>
2026-04-03 20:09:28 -0400

    add raw text strings

--- assignment4/part1/script.js
+++ assignment4/part1/script.js
@@ -15,18 +15,10 @@ function randomValueFromArray(array) {
 }
 
 // Raw text strings
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
+const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
-
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
 
 // Partial return random string function
 
