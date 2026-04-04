71038149a8b8e7e77f16ad0cbead21b7cda22fb2
mackinley-price <mackinley.price@dcmail.ca>
2026-04-04 17:56:16 -0400

    adds new evil circle object

--- assignment4/part4/script.js
+++ assignment4/part4/script.js
@@ -163,6 +163,11 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+const evilCircle = new EvilCircle(
+  random(0, width),
+  random(0, height)
+);
+
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
