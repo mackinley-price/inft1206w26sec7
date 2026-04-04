a6118b3f74c2dbd20496ddab186baaa05792fe70
mackinley-price <mackinley.price@dcmail.ca>
2026-04-04 17:51:14 -0400

    adds collision detection

--- assignment4/part4/script.js
+++ assignment4/part4/script.js
@@ -130,7 +130,19 @@ class EvilCircle extends Shape{
         this.y = this.size;
         }
     }
+    collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
 
+        if (distance < this.size + ball.size) {
+            ball.exists = false;
+        }
+      }
+    }
+  }
 }
 
 const balls = [];
