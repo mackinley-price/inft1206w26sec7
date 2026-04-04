2b68e8cd8fdd22134926a34123030857b94223f1
mackinley-price <mackinley.price@dcmail.ca>
2026-04-03 20:20:40 -0400

    adds random story string function

--- assignment4/part1/script.js
+++ assignment4/part1/script.js
@@ -23,7 +23,15 @@ const events = ["spontaneously combusted", "melted into a puddle on the sidewalk
 // Partial return random string function
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+    
+    let storyText=`It was 94 Fahrenheit outside, so ${insertx} 
+    went for a walk. When they got to ${inserty}, they stared in horror 
+    for a few moments, then ${insertz}. Bob saw the whole thing, but was 
+    not surprised — ${insertx} weighs 300 pounds, and it was a hot day.`;
+
+    const insertx = randomValueFromArray(characters);
+    const inserty = randomValueFromArray(places); 
+    const insertz = randomValueFromArray(events);
 
   return storyText;
 }
